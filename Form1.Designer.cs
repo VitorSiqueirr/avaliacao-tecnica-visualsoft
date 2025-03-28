bb420e3c6edc425b615ca51973dd126ce1088a39
Vitor Siqueira <vitor.siqueira@grupotelnet.com.br>
2025-03-28 17:32:35 -0400

    Iniciado o Singleton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1350, 545);
+            this.ClientSize = new System.Drawing.Size(753, 409);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
---
+++ SingletonBD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace avaliacao_tecnica_visualsoft
+{
+    public sealed class SingletonBD
+    {
+        private static readonly String strConnection = "server=localhost;port=3306;database=avaliacao-tecnica;uid=root;pwd=visualsoft;";        
+        private readonly MySqlConnection connection = new MySqlConnection(strConnection);
+        
+
+        public MySqlConnection Connection
+        {
+            get
+            {
+                return connection;
+            }
+        }
+    }
+}
