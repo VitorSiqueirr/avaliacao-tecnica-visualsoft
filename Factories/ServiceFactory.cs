f31b4195e38fbfabf4ef497e60992cdd29e98980
Vitor Siqueira <vitor.siqueira@grupotelnet.com.br>
2025-03-31 16:32:17 -0400

    Cleaned unused imports

--- Factories/ServiceFactory.cs
+++ Factories/ServiceFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using avaliacao_tecnica_visualsoft.Services;
+﻿using avaliacao_tecnica_visualsoft.Services;
 
 namespace avaliacao_tecnica_visualsoft.Factories
 {
